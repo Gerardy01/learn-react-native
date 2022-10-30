177bb1652663f4821423f69be131d41c3578e9bf
Gerardy Lucas <gerardy.lucas.p@gmail.com>
2022-10-30 16:29:45 +0700

    learn 1

--- App.js
+++ App.js
@@ -1,20 +1,99 @@
+import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet,
+  Text,
+  View, // == div
+  SafeAreaView,
+  StatusBar as PhoneStatusBar,
+  Platform,
+  Image,
+  TouchableWithoutFeedback, // for component that doesnt have onPress properties
+  TouchableOpacity, // when tapped, opacity beome reduced (effect when the component pressed)
+  TouchableHighlight,
+  TouchableNativeFeedback, // only for android!
+  Button,
+  Alert, // to customize alert (not element)
+} from 'react-native';
+
+import logo from './assets/favicon.png'
 
 export default function App() {
+
+  const [count, setCount] = useState(0);
+  
+  const handlePress = (e) => {
+    setCount(prev => prev + 1)
+  }
+
+  const handleButtonClick = () => {
+    Alert.alert('from button', 'hello world', [
+      {text: 'Yes', onPress: alert('kamu pintar')},
+      {text: 'No'}
+    ]);
+  }
+
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerText}>Gojek</Text>
+      </View>
+
+      <Text onPress={() => setCount(0)}>
+        Open up App.js to start working on your app!
+      </Text>
+
+      <Text
+        numberOfLines={2}
+        onPress={e => handlePress(e)}
+      >
+        Hello askldlasm dsaldlsam slakmdmlas dslamdlaskm sdlamdlaksm sdlamdl
+        asdjasldsajd lsdlsadls sajdlasjdls dlskadlsajdl dlsdjlasjdl dsldjls
+      </Text>
+
+      <Text>{count}</Text>
+
+      <TouchableOpacity onPress={() => console.log('touchable opacity')}>
+        <Image source={logo} />
+      </TouchableOpacity>
+
+      <TouchableHighlight onPress={() => console.log('touchable hilights')}>
+        <Image source={logo} />
+      </TouchableHighlight>
+
+      <TouchableNativeFeedback onPress={() => console.log('touchable native feedback (only for android)')}>
+        <View style={styles.test} />
+      </TouchableNativeFeedback>
+
+      <Button title='Button 1' onPress={handleButtonClick}/>
+      
+      <StatusBar style='auto' backgroundColor='rgb(0, 182, 0)' />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  header: {
+    width: '100%',
+    backgroundColor: 'rgb(0, 182, 0)',
+    padding: 10
+  },
+  headerText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 30
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? PhoneStatusBar.currentHeight : 0
+  },
+  test: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'pink',
   },
 });
